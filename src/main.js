1cbeefbd2af5d64cefa35d2ca7c253f883163ea6
ashwingupta99 <67099106+ashwingupta99@users.noreply.github.com>
2021-07-22 10:54:56 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 /** @type {import("../typings/phaser")} */
 
 /**
+Ashwin Gupta, Rocket Patrol Modded, 
  * Track a high score that persists across scenes and display it in the UI                5
  * Implement the 'FIRE' UI text from the original game                                    5
  * Implement the speed increase that happens after 30 seconds in the original game        5
  * Randomize each spaceship's movement direction at the start of each play                5
- * Create a new scrolling tile sprite for the background                                  5
  * Allow the player to control the Rocket after it's fired                                5
  * Add your own (copyright-free) background music to the Play scene                       5
  * Create a new title screen                                                             10
  * Display the time remaining (in seconds) on the screen                                 10
  * Create 4 new explosion SFX and randomize which one plays on impact                    10
- * -------------------------------------------------------                               65
+ * -------------------------------------------------------                               60
 **/
 
  let config={
